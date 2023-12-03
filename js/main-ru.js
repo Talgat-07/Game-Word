fe5a4fc4f9c8dff1e3fbe5ec1d0ce5545f130168
Talgat-07 <144517228+Talgat-07@users.noreply.github.com>
2023-12-03 14:24:20 +0600

    Update main-ru.js

--- js/main-ru.js
+++ js/main-ru.js
@@ -50,7 +50,7 @@ const gameOver = isVictory => {
 			'p'
 		).innerHTML = `${modalText} <b>${currentWord}</b>`;
 		gameModal.classList.add('show');
-	}, 300);
+	}, 500);
 };
 
 const initGame = (button, clickKedLetter) => {
